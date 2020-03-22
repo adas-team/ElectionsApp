82232c2ced3671d23fa5bb9b98cb7caa44bfbf91
Xinyue Xiang <xxiang2@ualberta.ca>
2020-03-21 18:55:29 -0600

    Revised two js name and changed the path for router (#38)
    * feat(context): add one python file to filter current json file
    
    * feat(context): add one page for filed validation and go back main
    
    * Delete filtered_mailList.json
    
    * Delete json_filter.py
    
    * feat(context): rename two pages and revise the path for router
    
    Co-authored-by: Helen <gezahegn@ualberta.ca>

---
+++ src/components/pages/IneligebleVoteScreen.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Main from "./components/pages/HomePage";
+
+class Vote extends Component {
+  constructor() {
+    super();
+    this.state = {
+      startVote: false,
+      quesNum: 1
+    };
+  }
+
+  startVote = () => {
+    this.setState({ startVote: true });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h1>Oooooops, seems your account didn't pass the validation. You can valid again!</h1>
+          {this.state.startVote ? (
+            <Main/>
+          ) : (
+            <button onClick={this.startVote}>Back to main page!</button>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Vote;
