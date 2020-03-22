2a936320209f0ce047e573f5936c9eb4d221dbcf
Xinyue Xiang <xxiang2@ualberta.ca>
2020-03-21 18:52:08 -0600

    feat(context): rename two pages and revise the path for router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Main from "./components/pages/HomePage";
 import Valid from "./components/pages/ValidateVoterPage";
-import Vote from "./components/pages/VotePage";
+import ValidPass from "./components/pages/EligebleVoteScreen";
+import ValidFail from "./components/pages/IneligebleVoteScreen";
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Main} />
       <Route exact path="/validate" component={Valid} />
-      <Route exact path="/vote" component={Vote} />
+      <Route exact path="/validpass" component={ValidPass} />
+      <Route exact path="/validfail" component={ValidFail} />
     </Switch>
   );
 }
