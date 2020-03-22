7806d3b95dc4261f72be9c53a73bf52cf9540295
Helen <gezahegn@ualberta.ca>
2020-03-22 14:21:35 -0600

    fix: max-width and min-height for home page segment items (#44)

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,10 +4,12 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import style from "styled-components";
 
-const ImageStyled = style(Image)`
-  position: fixed !important;
-  bottom: 0px;
+const ImageStyled = style.div`
   z-index: -1;
+  min-width: 834.656px;
+  max-height: 547px;
+  position: fixed !important;
+  overflow: hidden !important; 
 `;
 
 const WelcomeMessage = style.h1`
@@ -21,37 +23,46 @@ const Subheader = style.h2`
 
 const InfoText = style.h3`
   text-align: center;
+  font-size: 19px;
 `;
 
-// TODO: Fill with appropriate info (e.g. Aboriginal Treaty land, etc)
-const infoText = `Lorem Ipsum is simply dummy text of the printing 
-and typesetting industry. Lorem Ipsum has been the industry's standard 
-dummy text ever since the 1500s, when an unknown printer took a galley of
-type and scrambled it to make a type specimen book. It has
-survived not only five centuries, but also the leap into
-electronic typesetting, remaining essentially unchanged. It was
-popularised in the 1960s with the release of Letraset sheets
-containing Lorem Ipsum passages, and more recently with desktop
-publishing software like Aldus PageMaker including versions of
-Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not
-simply random text. Lorem Ipsum is simply dummy text of the printing 
-and typesetting industry. Lorem Ipsum has been the industry's standard 
-dummy text ever since the 1500s, when an unknown printer took a galley of
-type and scrambled it to make a type specimen book. It has
-survived not only five centuries, but also the leap into
-electronic typesetting.`;
+const SegmentStyled = style(Segment)`
+  margin-top: 20px !important;
+  margin-bottom: 0 !important;
+  min-width: 836.656px;  
+`;
+
+const covidAcknowledgment = `During this pandemic, we hope you and your loved ones 
+are all safe, healthy, and you are taking great care of yourself mentally 
+and physically. We are priviledged to be living in a first-world country 
+that has the resources and capacity to mitigrate the risk of COVID-19. 
+With combined effort, time, and patience, things will be back to normal 
+relatively soon. With that being said, we thank you for taking the time 
+to help us run as smoothly as possible during this sensitive time.`;
+
+const beforeYouBegin = `Before you begin, the University of Alberta 
+acknowledges that we are located on Treaty 6 territory, and respects 
+the histories, languages, and cultures of First Nations, Metis, Inuit, 
+and all First Peoples of Canada, whose presence continues to enrich 
+our vibrant community.
+`;
+
+const checkOutCandidates = `If you haven't already, please refer to the
+Candidates tab to see all who are running before making a decision.`;
 
 class HomePage extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      visible: true
+    };
   }
 
   render() {
     return (
       <Grid centered columns={3}>
         <Grid.Column>
-          <Segment attached>
+          <SegmentStyled attached>
             <Label as="a" color="blue" size="massive" ribbon>
               2020
             </Label>
@@ -62,14 +73,24 @@ class HomePage extends Component {
             <Subheader>
               We appreciate you all coming out to participate!
             </Subheader>
-            <InfoText>{infoText}</InfoText>
+            <InfoText>{covidAcknowledgment}</InfoText>
+            <InfoText>{beforeYouBegin}</InfoText>
+            <InfoText>{checkOutCandidates}</InfoText>
             <Link to="/validate">
-              <Button fluid attached="bottom" color="blue" size="massive">
+              <Button
+                onClick={this.handleButtonClick}
+                fluid
+                attached="bottom"
+                color="blue"
+                size="massive"
+              >
                 Start
               </Button>
             </Link>
-            <ImageStyled attached src={AdaBotHandsUpImage} />
-          </Segment>
+          </SegmentStyled>
+          <ImageStyled>
+            <Image attached src={AdaBotHandsUpImage} />
+          </ImageStyled>
         </Grid.Column>
       </Grid>
     );
