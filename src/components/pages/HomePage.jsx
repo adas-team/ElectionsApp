78fde68e432e598aa146b2104a41db0ad6fbb2c7
Helen <gezahegn@ualberta.ca>
2020-03-22 01:41:23 -0600

    feat: simple styling for home page (#41)

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,5 +1,45 @@
+import { Image, Grid, Label, Segment, Button } from "semantic-ui-react";
+import AdaBotHandsUpImage from "../../assets/AdaBotHandsUp.png";
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import style from "styled-components";
+
+const ImageStyled = style(Image)`
+  position: fixed !important;
+  bottom: 0px;
+  z-index: -1;
+`;
+
+const WelcomeMessage = style.h1`
+  font-size: 80px !important;
+  text-align: center;
+`;
+
+const Subheader = style.h2`
+  text-align: center;
+`;
+
+const InfoText = style.h3`
+  text-align: center;
+`;
+
+// TODO: Fill with appropriate info (e.g. Aboriginal Treaty land, etc)
+const infoText = `Lorem Ipsum is simply dummy text of the printing 
+and typesetting industry. Lorem Ipsum has been the industry's standard 
+dummy text ever since the 1500s, when an unknown printer took a galley of
+type and scrambled it to make a type specimen book. It has
+survived not only five centuries, but also the leap into
+electronic typesetting, remaining essentially unchanged. It was
+popularised in the 1960s with the release of Letraset sheets
+containing Lorem Ipsum passages, and more recently with desktop
+publishing software like Aldus PageMaker including versions of
+Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not
+simply random text. Lorem Ipsum is simply dummy text of the printing 
+and typesetting industry. Lorem Ipsum has been the industry's standard 
+dummy text ever since the 1500s, when an unknown printer took a galley of
+type and scrambled it to make a type specimen book. It has
+survived not only five centuries, but also the leap into
+electronic typesetting.`;
 
 class HomePage extends Component {
   constructor() {
@@ -9,15 +49,29 @@ class HomePage extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="jumbotron">
-          <h2>Welcome to Ada's Team Elections</h2>
-          <h4>WHO ARE WE? Ada's Team! </h4>
-          <h4>WHAT DO WE DO? Promote diversity! </h4>
-          <h4>WHERE DO WE DO IT? In technology!</h4>
-          <Link to="validate">Start</Link>
-        </div>
-      </div>
+      <Grid centered columns={3}>
+        <Grid.Column>
+          <Segment attached>
+            <Label as="a" color="blue" size="massive" ribbon>
+              2020
+            </Label>
+            <WelcomeMessage>
+              Welcome to <br />
+              Ada's Team Elections!
+            </WelcomeMessage>
+            <Subheader>
+              We appreciate you all coming out to participate!
+            </Subheader>
+            <InfoText>{infoText}</InfoText>
+            <Link to="/validate">
+              <Button fluid attached="bottom" color="blue" size="massive">
+                Start
+              </Button>
+            </Link>
+            <ImageStyled attached src={AdaBotHandsUpImage} />
+          </Segment>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
