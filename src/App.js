e5b37ad6391832b600f0bd3287198d5485ea193c
Helen <gezahegn@ualberta.ca>
2020-03-21 23:53:44 -0600

    Create menu bar (#40)
    * feat: add global header
    
    * fix: height of ValidateVoterPage
    - to make room for global header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
 import Main from "./components/pages/HomePage";
 import Valid from "./components/pages/ValidateVoterPage";
 import ValidPass from "./components/pages/EligibleVoterScreen";
@@ -8,12 +9,15 @@ import ValidFail from "./components/pages/IneligibleVoterScreen";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Main} />
-      <Route exact path="/validate" component={Valid} />
-      <Route exact path="/validpass" component={ValidPass} />
-      <Route exact path="/validfail" component={ValidFail} />
-    </Switch>
+    <Fragment>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/validate" component={Valid} />
+        <Route exact path="/validpass" component={ValidPass} />
+        <Route exact path="/validfail" component={ValidFail} />
+      </Switch>
+    </Fragment>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { Menu } from "semantic-ui-react";
+import { Redirect } from "react-router-dom";
+import AdaBotHead from "../assets/AdaBotHead.png";
+
+const ACTIVE_ITEM_PATHS = {
+  home: "/",
+  candidates: "/candidates",
+  results: "results"
+};
+
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activeItem: undefined,
+      activeItemPath: "",
+      redirect: false
+    };
+  }
+
+  handleItemClick = (e, { name }) => {
+    this.setState({
+      activeItem: name,
+      activeItemPath: ACTIVE_ITEM_PATHS[name],
+      redirect: true
+    });
+  };
+
+  render() {
+    const { activeItem, redirect, activeItemPath } = this.state;
+
+    if (redirect) {
+      this.setState({ redirect: false });
+      return <Redirect to={activeItemPath} />;
+    }
+
+    return (
+      <Menu size="massive" stackable inverted>
+        <Menu.Item href="https://adas-team.github.io/" target="_blank">
+          <img src={AdaBotHead} alt="Ada's Bot Head" />
+        </Menu.Item>
+
+        <Menu.Item
+          name="home"
+          active={activeItem === "home"}
+          onClick={this.handleItemClick}
+        >
+          Home
+        </Menu.Item>
+
+        <Menu.Item
+          name="candidates"
+          active={activeItem === "candidates"}
+          onClick={this.handleItemClick}
+        >
+          Candidates
+        </Menu.Item>
+
+        <Menu.Item
+          name="results"
+          active={activeItem === "results"}
+          onClick={this.handleItemClick}
+        >
+          Results
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+export default Header;
