300f360748c89b33a6a1f81d5b2311a58aa7773b
Helen Gezahegn <gezahegn@ualberta.ca>
2020-03-26 16:27:12 -0600

    feat: voting for candidate

---
+++ src/components/votepart/ListCandidates.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Candidate from "./Candidate";
+import { getCandidateList } from "../helper";
+import { Form, Card } from "semantic-ui-react";
+
+class ListCandidates extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      candidates: {},
+      currSelection: ""
+    };
+  }
+
+  componentDidMount() {
+    this.setCandidateList();
+  }
+
+  setCandidateList = async () => {
+    const candidates = await getCandidateList();
+    this.setState({ candidates });
+  };
+
+  onSelect = name => {
+    this.setState({ currSelection: name });
+  };
+
+  renderCandidates = position => {
+    const { candidates, currSelection } = this.state;
+    if (candidates[position]) {
+      const currCandidates = candidates[position];
+      return Object.keys(currCandidates).map(candidateName => (
+        <Candidate
+          position={position}
+          key={candidateName}
+          candidateName={candidateName}
+          currSelection={currSelection}
+          onSelect={this.onSelect}
+        />
+      ));
+    }
+  };
+
+  render() {
+    const { position } = this.props;
+    return (
+      <Form>
+        <Form.Group grouped widths="equal">
+          <Card.Group>{this.renderCandidates(position)}</Card.Group>
+        </Form.Group>
+      </Form>
+    );
+  }
+}
+
+export default ListCandidates;
