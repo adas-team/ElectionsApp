1d66dc54845c0825ce749a3fca4243bc71cb5937
Helen Gezahegn <gezahegn@ualberta.ca>
2020-03-29 01:07:43 -0600

    feat: load results screen - getWinners from firestore - render results

--- src/components/pages/Results.jsx
+++ src/components/pages/Results.jsx
@@ -21,6 +21,12 @@ const PlaceholderContainer = style(Placeholder)`
   height: 400px !important;
 `;
 
+const Container = style.div`
+  display: flex !important;
+  font-size: 18px !important;
+  justify-content: center !important;
+`;
+
 class Results extends Component {
   constructor() {
     super();
