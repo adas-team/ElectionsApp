ef89b54b2b3968238d4961c9a945a052180e9f06
Helen Gezahegn <gezahegn@ualberta.ca>
2020-04-02 22:17:55 -0600

    feat: popup message trigger and voter page description (#74)
    * feat: added description to hover position name to get details
    
    * fix: trigger popup only when text is hovered, not container

--- src/components/VotePart/Position.jsx
+++ src/components/VotePart/Position.jsx
@@ -7,9 +7,18 @@ const PopupMessage = style(Popup)`
   font-size: 16px !important;
   max-width: 62.5% !important; 
 `;
+
+const PositionName = style.h1`
+  width: fit-content;
+`;
+
 const Position = ({ name }) => {
   return (
-    <PopupMessage wide="very" trigger={<h1>{name}</h1>} position="top left">
+    <PopupMessage
+      wide="very"
+      trigger={<PositionName>{name}</PositionName>}
+      position="top left"
+    >
       {positionDesc[0][name]}
     </PopupMessage>
   );
